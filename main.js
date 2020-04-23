e621c88f527cb3985aff7a1d99448047c8b57a01
rmateu <rmateusemis@gmail.com>
2020-04-23 12:50:07 +0200

    OK

---
+++ main.js
@@ -0,0 +1,67 @@
+/*
+// Exercici01
+console.log("Hola mundo");
+
+
+// Exercici02
+alert("¡Me llamo Roger!");
+
+
+// Exercici 03
+var name = "Roger"
+var lastName = "Mateu"
+// Exercici 04
+var num1 = 6;
+var num2 = 4;
+
+console.log("La suma entre "+num1+" y "+num2+" es "+(num1+num2));
+
+
+// Exercici 05
+var nota_examen = 7.5;
+
+if (nota_examen < 5) {
+  alert("Has suspendido con un "+nota_examen)
+
+} else {
+  alert("Has aprovado con un "+nota_examen)
+}
+
+
+// Exercici 06
+var text = "Tinc un cotxe de color verd";
+console.log(text);
+
+text.replace(/verd/, 'blau');
+console.log(text.replace(/o/g, 'u'));
+
+
+// Exercici 07
+var objects = ["taula", "cadira", "ordinador", "llibreta"];
+console.log(objects);
+
+for (var i = 0; i < objects.length; i++) {
+  console.log("L'objecte "+objects[i]+" esta a la posició "+i)
+}
+
+
+// Exercici 08
+function calculadora(operador,valor1,valor2){
+  switch (operador) {
+    case "suma":
+    document.write((valor1+valor2));
+    break;
+
+    case "resta":
+    document.write((valor1-valor2));
+    break;
+
+    case "multiplicacio":
+    document.write((valor1*valor2));
+    break;
+
+    default:
+  }
+}
+var resultat = calculadora("resta", 10, 60);
+*/
